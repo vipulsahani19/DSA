4f928803207a6d4249b576a946601864e7ec43f4
Vipul Sahani <vipulsahani2006@gmail.com>
2026-07-20 11:12:44 +0530

    Time: 31 ms (26.13%) | Memory: 48.7 MB (80.69%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -0,0 +1,57 @@
+class Solution {
+    static int largestRectangleArea(int[] arr) {
+        int n=arr.length;
+        int nes[]=new int [n];
+        int pse[]=new int [n];
+        nes[n-1]=n;
+        Stack<Integer> st=new Stack<>();
+        st.push(n-1);
+        for(int i=n-2;i>=0;i--){
+            while(!st.isEmpty() && arr[st.peek()]>=arr[i]){
+                st.pop();
+            }
+            if(st.isEmpty()) nes[i]=n;
+            else nes[i]=st.peek();
+            st.push(i);
+        }
+        while(!st.isEmpty()) st.pop();
+        pse[0]=-1;
+        st.push(0);
+        for(int i=1;i<n;i++){
+            while(!st.isEmpty() && arr[st.peek()]>=arr[i]){
+                st.pop();
+            }
+            if(st.isEmpty()) pse[i]=-1;
+            else pse[i]=st.peek();
+            st.push(i);
+        }
+        int max=0;
+        for(int i=0;i<n;i++){
+            int area=arr[i]*(nes[i]-pse[i]-1);
+            max=Math.max(max,area);
+        } return max;
+    }
+    public int maximalRectangle(char[][] arr) {
+        int n=arr.length;
+        int m=arr[0].length;
+        int ans[][]=new int[n][m];
+        for(int j=0;j<m;j++){
+            for(int i=0;i<n;i++){
+                if (arr[i][j] == '1') {
+                    if (i == 0) {
+                        ans[i][j] = 1;
+                    } else {
+                    ans[i][j] = ans[i - 1][j] + 1;
+                    }
+                }
+            }
+        }
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, largestRectangleArea(ans[i]));
+        }
+
+        return max;
+    }
+}
