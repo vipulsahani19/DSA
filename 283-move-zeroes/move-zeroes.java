9a4a4df4b5f8a96654ea773f48b34dfc02e25c42
Vipul Sahani <vipulsahani2006@gmail.com>
2026-07-28 21:49:48 +0530

    Time: 2 ms (92.11%) | Memory: 47.7 MB (76.31%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i=0;
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=0){
+                if(i!=j){
+                    int temp=nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=temp;
+                }
+                    i++;
+            }
+        }
+
+    }
+}
