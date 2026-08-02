852e798601d05cb9f0eb42b787004b6609cbfc74
Vipul Sahani <vipulsahani2006@gmail.com>
2026-08-02 22:26:33 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (54.16%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -15,20 +15,34 @@
  */
 class Solution {
     public void flatten(TreeNode root) {
-        if(root==null) return ;
-        ArrayList<TreeNode> arr=new ArrayList<>();
-        dfs(root,arr);
-        root=arr.get(0);
-        for(int i=1;i<arr.size();i++){
-            root.right=arr.get(i);
-            root.left=null;
-            root=root.right;
+        // if(root==null) return ;
+        // ArrayList<TreeNode> arr=new ArrayList<>();
+        // dfs(root,arr);
+        // root=arr.get(0);
+        // for(int i=1;i<arr.size();i++){
+        //     root.right=arr.get(i);
+        //     root.left=null;
+        //     root=root.right;
+        // }
+        if(root==null) return;
+        TreeNode lst=root.left;
+        TreeNode rst=root.right;
+        root.left=null;
+        root.right=null;
+        flatten(lst);
+        flatten(rst);
+        root.right=lst;
+        TreeNode last=root;
+        while(last.right!=null){
+            last=last.right;
         }
+        last.right=rst;
+
     }
-    static void dfs(TreeNode root,ArrayList<TreeNode> arr){
-        if(root==null)return;
-        arr.add(root);
-        dfs(root.left,arr);
-        dfs(root.right,arr);
-    }
+    // static void dfs(TreeNode root,ArrayList<TreeNode> arr){
+    //     if(root==null)return;
+    //     arr.add(root);
+    //     dfs(root.left,arr);
+    //     dfs(root.right,arr);
+    // }
 }
