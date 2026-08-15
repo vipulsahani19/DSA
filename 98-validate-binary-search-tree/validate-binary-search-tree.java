d9785b9290b894f1da696efc6d847d7a0cc99cba
Vipul Sahani <vipulsahani2006@gmail.com>
2026-08-16 00:12:36 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (22.25%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -14,31 +14,33 @@
  * }
  */
 class Solution {
-    static boolean flag=true;
-    long maxleft(TreeNode root){
-        if(root==null) return Long.MIN_VALUE;
-        long left=maxleft(root.left);
-        long right=maxleft(root.right);
-        return Math.max(root.val,Math.max(left,right));
-    }
-    long minright(TreeNode root){
-        if(root==null) return Long.MAX_VALUE;
-        long left=minright(root.left);
-        long right=minright(root.right);
-        return Math.min(root.val,Math.min(left,right));
-    }
-    void helper(TreeNode root){
-        if(root==null) return;
-        long max=maxleft(root.left);
-        long min=minright(root.right);
-        if(max>=root.val || min<=root.val) flag=false;
-        helper(root.left);
-        helper(root.right);
+    // static boolean flag=true;
+    // long maxleft(TreeNode root){
+    //     if(root==null) return Long.MIN_VALUE;
+    //     long left=maxleft(root.left);
+    //     long right=maxleft(root.right);
+    //     return Math.max(root.val,Math.max(left,right));
+    // }
+    // long minright(TreeNode root){
+    //     if(root==null) return Long.MAX_VALUE;
+    //     long left=minright(root.left);
+    //     long right=minright(root.right);
+    //     return Math.min(root.val,Math.min(left,right));
+    // }
+    // void helper(TreeNode root){
+    //     if(root==null) return;
+    //     long max=maxleft(root.left);
+    //     long min=minright(root.right);
+    //     if(max>=root.val || min<=root.val) flag=false;
+    //     helper(root.left);
+    //     helper(root.right);
+    // }
+    boolean helper(TreeNode root,long min,long max){
+        if(root==null) return true;
+        if(min>=root.val || root.val>=max) return false;
+        return helper(root.left,min,root.val) && helper(root.right,root.val,max); 
     }
     public boolean isValidBST(TreeNode root) {
-        flag=true;
-        if(root.left==null && root.right==null) return true;
-        helper(root);
-        return flag;
+        return helper(root,Long.MIN_VALUE,Long.MAX_VALUE);
     }
 }
