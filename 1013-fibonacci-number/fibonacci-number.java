1430afd2fe8b38a6830ab49bb2c39d2a6791879f
Vipul Sahani <vipulsahani2006@gmail.com>
2026-08-25 17:56:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (16.04%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -7,8 +7,16 @@ class Solution {
         return ans;
     }
     public int fib(int n) {
-        int[] arr=new int [n+1];
-        
-        return helper(n,arr);
+        // int[] arr=new int [n+1];
+        if(n<=1) return n;
+        int a=0;
+        int b=1;
+        for(int i=0;i<n-1;i++){
+            int c=a+b;
+            a=b;
+            b=c;
+        }
+        return b;
+        // return helper(n,arr);
     }
 }
