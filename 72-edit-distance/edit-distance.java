cc518f03b8a7a5710532c779d389460de03833fe
Vipul Sahani <vipulsahani2006@gmail.com>
2026-09-13 14:54:59 +0530

    Time: 5 ms (65.64%) | Memory: 47.2 MB (52.43%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -3,7 +3,7 @@ class Solution {
         int n=s1.length();
         int m=s2.length();
         int[][] dp=new int [n][m];
-        for(int i=0;i<n;i++) Arrays.fill(dp[i],-1);
+        // for(int i=0;i<n;i++) Arrays.fill(dp[i],-1);
         return helper(n-1,m-1,s1,s2,dp);
     }
     int helper(int i,int j,String s1,String s2,int[][] dp){
@@ -12,7 +12,7 @@ class Solution {
         if(s1.charAt(i)==s2.charAt(j)){
             return helper(i-1,j-1,s1,s2,dp);
         }
-        if(dp[i][j]!=-1) return dp[i][j];
+        if(dp[i][j]!=0) return dp[i][j];
         // replace
         int replace=1+helper(i-1,j-1,s1,s2,dp);
         // remove 
